929241357dfb28b3c6ce6ffa7a9a5ee9fbaf5c4a
Josh Morel <morel.josh@hotmail.com>
2015-01-14 07:18:10 -0500

    Modified view to include day of week abbreviation

--- SQL/Views/dboFactEvent_By_DateAdded_VR.sql
+++ SQL/Views/dboFactEvent_By_DateAdded_VR.sql
@@ -18,7 +18,7 @@ SELECT
 	 AD.Date AS AddedDate
 	 ,AD.Year AS AddedYear
 	 ,AD.Month AS AddedMonth
-	 ,AD.DayName as [DayOfWeek]
+	 ,LEFT(AD.DayName,3) as [DayOfWeek]
 	,COUNT(*) AS RecordCount
     
   FROM dbo.FactEvent F
@@ -28,7 +28,7 @@ GROUP BY
 	 AD.Date
 	 ,AD.Year
 	 ,Ad.Month
-	 ,AD.DayName
+	 ,LEFT(AD.DayName,3)
 
 
 
