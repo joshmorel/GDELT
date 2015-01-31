bb4cf66438966ef4d05b6c336d0a175e80f48925
Josh Morel <morel.josh@hotmail.com>
2015-01-31 10:03:06 -0500

    Added sequences

---
+++ SQL/Other/dboGeoNameKey.sql
@@ -0,0 +1,16 @@
+USE [GDELT]
+GO
+
+USE [GDELT]
+GO
+
+/****** Object:  Sequence [dbo].[SeqGeoNameKey]    Script Date: 31/01/2015 10:02:22 AM ******/
+CREATE SEQUENCE [dbo].[SeqGeoNameKey] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ CACHE 
+GO
+
---
+++ SQL/Other/dboSeqActorKey.sql
@@ -0,0 +1,16 @@
+USE [GDELT]
+GO
+
+USE [GDELT]
+GO
+
+/****** Object:  Sequence [dbo].[SeqActorKey]    Script Date: 31/01/2015 10:01:53 AM ******/
+CREATE SEQUENCE [dbo].[SeqActorKey] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ CACHE 
+GO
+
