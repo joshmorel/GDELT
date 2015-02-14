8b724e820cda5b4a3197757c764e28f9576c0a32
Josh Morel <morel.josh@hotmail.com>
2015-02-14 07:14:29 -0500

    Modified date dimension to align with what I am using GRH

---
+++ SQL/StoredProcedures/dboPopulateDimDate.sql
@@ -0,0 +1,194 @@
+USE [GDELT]
+GO
+
+/****** Object:  StoredProcedure [dbo].[PopulateDimDate]    Script Date: 2015-02-14 07:13:48 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+
+
+-- =============================================
+-- Author:		Josh Morel
+-- Create date: 2015-02-01
+-- Description:	Insert geography dimension with transactional data
+-- =============================================
+CREATE PROCEDURE [dbo].[PopulateDimDate]
+
+AS
+BEGIN
+SET NOCOUNT ON;
+
+truncate table dbo.DimDate;
+
+--Purpose: To populate the DIM_DATE table
+
+--Step 1: Declare temporary holiday table and populate with data from ESP holiday table
+--Note: Data available for FY 2004/05 forward. Updated yearly.
+
+--Step 2: Declare temporary date table 
+DECLARE @DIM_DATE TABLE (
+	DATE_KEY INT
+	,FULL_DATE DATETIME2(0)
+	,DATE_YYYYMMDD VARCHAR(10)
+	,DATE_MMDDYYYY VARCHAR(10)
+	,DATE_DDMMYYYY VARCHAR(10)
+	,DAY_OF_WEEK SMALLINT
+	,DAY_OF_WEEK_NAME VARCHAR(10)
+	,DAY_OF_WEEK_ABBREVIATION VARCHAR(3)
+	,DAY_OF_MONTH SMALLINT
+	,DAY_OF_YEAR SMALLINT
+
+	,WEEKDAY_WEEKEND VARCHAR(7)
+	,IS_WEEKDAY TINYINT
+
+	,WEEK_OF_YEAR TINYINT
+	,MONTH_NAME VARCHAR(10)
+	,MONTH_ABBREVIATION VARCHAR(3)
+	,MONTH_OF_YEAR SMALLINT
+	,IS_LAST_DAY_OF_MONTH TINYINT
+	,CALENDAR_QUARTER SMALLINT
+	,CALENDAR_QUARTER_NAME VARCHAR(3)
+	,CALENDAR_YEAR SMALLINT
+	,CALENDAR_YEAR_NAME VARCHAR(7)
+	,CALENDAR_YEAR_MONTH VARCHAR(16)
+	,CALENDAR_YEAR_QUARTER VARCHAR(12)
+	,FISCAL_MONTH_OF_YEAR SMALLINT
+	,FISCAL_QUARTER SMALLINT
+	,FISCAL_QUARTER_NAME VARCHAR(3)
+	,FISCAL_YEAR SMALLINT
+	,FISCAL_YEAR_NAME VARCHAR(10)
+	,FISCAL_YEAR_MONTH VARCHAR(19)
+	,FISCAL_YEAR_QUARTER VARCHAR(15)
+	,LAST_DAY_OF_WEEK DATETIME2(0)
+	,LAST_DAY_OF_MONTH DATETIME2(0)
+);
+
+--Step 3: Populate date table with days from 1950 to 2050
+DECLARE @DATECTR DATETIME2(0) = '1950-01-01 00:00';
+
+WHILE @DATECTR <= '2050-12-31'
+
+BEGIN
+INSERT INTO dbo.DimDate
+VALUES(
+	CAST(CONVERT(CHAR(8),@DATECTR,112) AS INT) --Date Key
+	,@DATECTR --Date in Date Format
+	,CONVERT(CHAR(10),@DATECTR,111) --Date in YYYY/MM/DD format (Japan)
+	,CONVERT(CHAR(10),@DATECTR,101) --Date in MM/DD/YYYY format (US)
+	,CONVERT(CHAR(10),@DATECTR,103) --Date in DD/MM/YYYY format (UK)
+	,DATEPART(DW,@DATECTR) --Day of Week (number)
+	,DATENAME(DW,@DATECTR) --Day Name (full)
+	,LEFT(DATENAME(DW,@DATECTR),3) --Day Name (abbreviation)
+	,DAY(@DATECTR) --Day of Month
+	,DATEPART(DY,@DATECTR) --Day of Year
+	,CASE WHEN DATEPART(DW,@DATECTR) IN (1,7) THEN 'Weekend' ELSE 'Weekday' END --Weekend/Weekday
+	,CASE WHEN DATEPART(DW,@DATECTR) IN (1,7) THEN 0 ELSE 1 END --Is Weekend (boolean)
+	,DATEPART(WEEK,@DATECTR) --Week of Year
+	,DATENAME(M,@DATECTR) --Month Name (full)
+	,LEFT(DATENAME(M,@DATECTR),3) --Month Name (abbreviation)
+	,MONTH(@DATECTR) --Month of Year
+	,CASE WHEN MONTH(@DATECTR) <> MONTH(DATEADD(DAY,1,@DATECTR)) THEN 1 ELSE 0 END --Is Last Day of Month (boolean)
+	,DATEPART(QQ,@DATECTR) --Calendar Quarter (number) 
+	,'Q'+DATENAME(QQ,@DATECTR) --Calendar Quarter (name)
+	,YEAR(@DATECTR) --Calendar Year (quarter)
+	,'CY '+DATENAME(YYYY,@DATECTR) --Calendar Year (name)
+	,'CY '+DATENAME(YYYY,@DATECTR) + ' ' + RIGHT('0'+CAST(MONTH(@DATECTR) AS VARCHAR(2)),2) + '_' + LEFT(DATENAME(M,@DATECTR),3) --Calendar Year & Month
+	,'CY '+DATENAME(YYYY,@DATECTR) + ' Q' + DATENAME(QQ,@DATECTR) --Calendar Year & Quarter
+	,CASE WHEN MONTH(@DATECTR) <= 3 THEN MONTH(@DATECTR) + 9 ELSE MONTH(@DATECTR) - 3 END --Fiscal Year of Month
+	,CASE WHEN MONTH(@DATECTR) <= 3 THEN 4 ELSE DATEPART(QQ,@DATECTR) - 1 END --Fiscal Quarter (number)
+	,CASE WHEN MONTH(@DATECTR) <= 3 THEN 'Q4' ELSE 'Q'+CAST(DATEPART(QQ,@DATECTR) - 1 AS CHAR(1)) END --Fiscal Quarter (name)
+	,CASE WHEN MONTH(@DATECTR) <= 3 THEN YEAR(@DATECTR)-1 ELSE YEAR(@DATECTR) END --Fiscal year (number)
+	,CASE WHEN MONTH(@DATECTR) <= 3 
+		THEN 'FY ' + CAST(YEAR(@DATECTR)-1 AS CHAR(4))+'/'+RIGHT(DATENAME(YYYY,@DATECTR),2) 
+		ELSE 'FY ' + DATENAME(YYYY,@DATECTR) + '/' + RIGHT(CAST(YEAR(@DATECTR)+1 AS CHAR(4)),2) 
+	END --Fiscal Year (name)
+	,CASE WHEN MONTH(@DATECTR) <= 3 
+		THEN 'FY ' + CAST(YEAR(@DATECTR)-1 AS CHAR(4))+'/'+RIGHT(DATENAME(YYYY,@DATECTR),2) + ' ' + 
+			CAST(MONTH(@DATECTR) + 9 AS CHAR(2)) + '_' + LEFT(DATENAME(M,@DATECTR),3)
+		ELSE 'FY ' + DATENAME(YYYY,@DATECTR) + '/' + RIGHT(CAST(YEAR(@DATECTR)+1 AS CHAR(4)),2) + ' ' + 
+			RIGHT('0'+CAST(MONTH(@DATECTR) - 3 AS VARCHAR(2)),2) + '_' + LEFT(DATENAME(M,@DATECTR),3)
+	END	--Fiscal Year & Month
+
+	,CASE WHEN MONTH(@DATECTR) <= 3 
+		THEN 'FY ' + CAST(YEAR(@DATECTR)-1 AS CHAR(4))+'/'+RIGHT(DATENAME(YYYY,@DATECTR),2) + ' Q4'
+		ELSE 'FY ' + DATENAME(YYYY,@DATECTR) + '/' + RIGHT(CAST(YEAR(@DATECTR)+1 AS CHAR(4)),2) + ' ' 
+			+ 'Q'+CAST(DATEPART(QQ,@DATECTR) - 1 AS CHAR(1))
+	END	--Fiscal Year & Quarter
+	,DATEADD(DAY,7-DATEPART(DW,@DATECTR),@DATECTR) --End of Week (Saturday of same week)
+	,DATEADD(DAY,-DAY(DATEADD(MONTH,1,@DATECTR)),DATEADD(MONTH,1,@DATECTR)) --End of Month
+)
+
+SET @DATECTR = DATEADD(DAY,1,@DATECTR );
+END
+
+;
+
+
+--Step 5: Insert unknown date record
+INSERT INTO dbo.DimDate
+VALUES
+(
+	30001231
+	,'3000-12-31'
+	,'N/A'
+	,'N/A'
+	,'N/A'
+	,null
+	,'N/A'
+	,'N/A'
+	,null
+	,null
+
+	,'N/A'
+	,null
+	,null
+	,'N/A'
+	,'N/A'
+	,null
+	,null
+	,null
+	,'N/A'
+	,null
+	,'N/A'
+	,'N/A'
+	,'N/A'
+	,null
+	,null
+	,'N/A'
+	,null
+	,'N/A'
+	,'N/A'
+	,'N/A'	
+	,'3000-12-31'
+	,'3000-12-31'
+)
+
+
+END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GO
+
--- SQL/Tables/dboDimDate.sql
+++ SQL/Tables/dboDimDate.sql
@@ -1,7 +1,7 @@
 USE [GDELT]
 GO
 
-/****** Object:  Table [dbo].[DimDate]    Script Date: 13/01/2015 6:49:44 PM ******/
+/****** Object:  Table [dbo].[DimDate]    Script Date: 2015-02-14 07:13:33 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,61 +12,41 @@ SET ANSI_PADDING ON
 GO
 
 CREATE TABLE [dbo].[DimDate](
-	[DateKey] [int] NOT NULL,
-	[Date] [datetime2](0) NULL,
-	[FullDateUK] [char](10) NULL,
-	[FullDateUSA] [char](10) NULL,
-	[DayOfMonth] [int] NULL,
-	[BusinessDayOfMonth] [int] NULL,
-	[DaySuffix] [varchar](4) NULL,
-	[DayName] [varchar](9) NULL,
-	[DayOfWeekUSA] [char](1) NULL,
-	[DayOfWeekUK] [char](1) NULL,
-	[DayOfWeekInMonth] [varchar](2) NULL,
-	[DayOfWeekInYear] [varchar](2) NULL,
-	[DayOfQuarter] [varchar](3) NULL,
-	[DayOfYear] [varchar](3) NULL,
-	[WeekOfMonth] [varchar](1) NULL,
-	[WeekOfQuarter] [varchar](2) NULL,
-	[WeekOfYear] [varchar](2) NULL,
-	[Month] [varchar](2) NULL,
-	[MonthName] [varchar](9) NULL,
-	[MonthOfQuarter] [varchar](2) NULL,
-	[Quarter] [char](1) NULL,
-	[QuarterName] [varchar](9) NULL,
-	[Year] [char](4) NULL,
-	[YearName] [char](7) NULL,
-	[MonthYear] [char](10) NULL,
-	[MMYYYY] [char](6) NULL,
-	[FirstDayOfMonth] [date] NULL,
-	[LastDayOfMonth] [date] NULL,
-	[FirstDayOfQuarter] [date] NULL,
-	[LastDayOfQuarter] [date] NULL,
-	[FirstDayOfYear] [date] NULL,
-	[LastDayOfYear] [date] NULL,
-	[IsHolidayUSA] [bit] NULL,
-	[IsWeekday] [bit] NULL,
-	[HolidayUSA] [varchar](50) NULL,
-	[IsHolidayUK] [bit] NULL,
-	[HolidayUK] [varchar](50) NULL,
-	[FiscalDayOfYear] [varchar](3) NULL,
-	[FiscalWeekOfYear] [varchar](3) NULL,
-	[FiscalMonth] [varchar](2) NULL,
-	[FiscalQuarter] [char](1) NULL,
-	[FiscalQuarterName] [varchar](9) NULL,
-	[FiscalYear] [char](4) NULL,
-	[FiscalYearName] [char](7) NULL,
-	[FiscalMonthYear] [char](10) NULL,
-	[FiscalMMYYYY] [char](6) NULL,
-	[FiscalFirstDayOfMonth] [date] NULL,
-	[FiscalLastDayOfMonth] [date] NULL,
-	[FiscalFirstDayOfQuarter] [date] NULL,
-	[FiscalLastDayOfQuarter] [date] NULL,
-	[FiscalFirstDayOfYear] [date] NULL,
-	[FiscalLastDayOfYear] [date] NULL,
- CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED 
+	[DATE_KEY] [int] NOT NULL,
+	[FULL_DATE] [datetime2](0) NOT NULL,
+	[DATE_YYYYMMDD] [varchar](10) NULL,
+	[DATE_MMDDYYYY] [varchar](10) NULL,
+	[DATE_DDMMYYYY] [varchar](10) NULL,
+	[DAY_OF_WEEK] [smallint] NULL,
+	[DAY_OF_WEEK_NAME] [varchar](10) NULL,
+	[DAY_OF_WEEK_ABBREVIATION] [varchar](3) NULL,
+	[DAY_OF_MONTH] [smallint] NULL,
+	[DAY_OF_YEAR] [smallint] NULL,
+	[WEEKDAY_WEEKEND] [varchar](7) NULL,
+	[IS_WEEKDAY] [tinyint] NULL,
+	[WEEK_OF_YEAR] [tinyint] NULL,
+	[MONTH_NAME] [varchar](10) NULL,
+	[MONTH_ABBREVIATION] [varchar](3) NULL,
+	[MONTH_OF_YEAR] [smallint] NULL,
+	[IS_LAST_DAY_OF_MONTH] [tinyint] NULL,
+	[CALENDAR_QUARTER] [smallint] NULL,
+	[CALENDAR_QUARTER_NAME] [varchar](3) NULL,
+	[CALENDAR_YEAR] [smallint] NULL,
+	[CALENDAR_YEAR_NAME] [varchar](7) NULL,
+	[CALENDAR_YEAR_MONTH] [varchar](16) NULL,
+	[CALENDAR_YEAR_QUARTER] [varchar](12) NULL,
+	[FISCAL_MONTH_OF_YEAR] [smallint] NULL,
+	[FISCAL_QUARTER] [smallint] NULL,
+	[FISCAL_QUARTER_NAME] [varchar](3) NULL,
+	[FISCAL_YEAR] [smallint] NULL,
+	[FISCAL_YEAR_NAME] [varchar](10) NULL,
+	[FISCAL_YEAR_MONTH] [varchar](19) NULL,
+	[FISCAL_YEAR_QUARTER] [varchar](15) NULL,
+	[LAST_DAY_OF_WEEK] [datetime2](0) NOT NULL,
+	[LAST_DAY_OF_MONTH] [datetime2](0) NOT NULL,
+ CONSTRAINT [PK_DATE_DIMENSION] PRIMARY KEY CLUSTERED 
 (
-	[DateKey] ASC
+	[DATE_KEY] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
